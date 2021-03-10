29628fa88ab3c6b9159d111ac9f8fdeba8b967a4
Andre Venancio <info@andrevenancio.com>
2021-03-10 21:50:06 +0000

    adding new footer notes

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,7 +4,11 @@ import styles from './index.module.scss'
 export const Footer = () => {
   return (
     <footer className={styles.container}>
-      sync -&gt; collect // sync -&gt; mint // sync -&gt; curate
+      <p>sync -&gt; collect // sync -&gt; mint // sync -&gt; curate</p>
+      <p>
+        use it consciously. visit artists profiles. be careful with copy
+        minters.
+      </p>
     </footer>
   )
 }
