fc028a0503db3a5bcf9353d9dd87e2db3a9f0b9a
Andre Venancio <info@andrevenancio.com>
2021-03-10 14:12:44 +0000

    fixing footer component

---
+++ src/components/footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+export const Footer = () => {
+  return (
+    <footer className={styles.container}>
+      sync -&gt; collect // sync -&gt; mint // sync -&gt; curate
+    </footer>
+  )
+}
