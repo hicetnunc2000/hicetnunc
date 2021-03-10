f09872b06981cfce8d7c9d1ac3adeb1a79036830
Andre Venancio <info@andrevenancio.com>
2021-03-10 11:43:37 +0000

    adding 3d player

--- src/components/media-types/index.js
+++ src/components/media-types/index.js
@@ -1,24 +1,34 @@
 import React from 'react'
+import { GLBComponent } from './glb'
 import { ImageComponent } from './image'
 import { VideoComponent } from './video'
 
 export const renderMediaType = (token_info) => {
   const { mimeType, uri } = token_info.formats[0]
+  const path = uri.split('//')[1]
   let url
 
   switch (mimeType) {
+    /* IMAGES */
     case 'image/gif':
     case 'image/jpeg':
     case 'image/png':
     case 'image/svg+xml':
     case 'image/bmp':
-      url = `https://cloudflare-ipfs.com/ipfs/${uri.split('//')[1]}`
+      url = `https://cloudflare-ipfs.com/ipfs/${path}`
       return <ImageComponent src={url} />
+    /* VIDEOS */
     case 'video/mp4':
     case 'video/ogg':
     case 'video/quicktime':
-      url = `https://dweb.link/ipfs/${uri.split('//')[1]}`
+      url = `https://dweb.link/ipfs/${path}`
       return <VideoComponent src={url} />
+    /* 3D */
+    case 'model/gltf-binary':
+    case 'model/gltf+json':
+      // TODO: @crzypatchwork to define which domain we extract this from
+      url = require('../../media/cube.glb')
+      return <GLBComponent src={url} />
     default:
       console.warn('non supported mimeType', mimeType)
       return null
