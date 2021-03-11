2a8f9110ee25329856d142d8bfea9c692c971334
[._.]/ Adam Eivy <70015+atomantic@users.noreply.github.com>
2021-03-11 11:17:04 -0800

    bring back lowest price swapping

--- src/components/item-info/index.js
+++ src/components/item-info/index.js
@@ -4,6 +4,7 @@ import { Button, Primary, Purchase } from '../button'
 import { HicetnuncContext } from '../../context/HicetnuncContext'
 import { walletPreview } from '../../utils/string'
 import styles from './index.module.scss'
+import { lowestPrice } from '../../utils/lowestPrice'
 
 export const ItemInfo = ({
   token_id,
@@ -15,15 +16,16 @@ export const ItemInfo = ({
   const context = useContext(HicetnuncContext)
 
   const notForSale = swaps.length === 0
-  const price = swaps.length > 0 && Number(swaps[0].xtz_per_objkt) / 1000000
-  const edition = swaps.length && `${swaps[0].objkt_amount}/${total_amount}`
+  const swap = lowestPrice(swaps)
+  const price = swaps.length > 0 && Number(swap.xtz_per_objkt) / 1000000
+  const edition = swaps.length && `${swap.objkt_amount}/${total_amount}`
   const message = notForSale ? 'not for sale' : `collect for ${price} tez`
 
   const handleCollect = () => {
     if (context.Tezos == null) {
       context.syncTaquito()
     } else {
-      context.collect(1, swaps[0].swap_id, swaps[0].xtz_per_objkt * 1)
+      context.collect(1, swap.swap_id, swap.xtz_per_objkt * 1)
     }
   }
 
