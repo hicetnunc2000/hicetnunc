0817a314592eaf0f72f645c0aea1d9ac879b9508
Andre Venancio <info@andrevenancio.com>
2021-03-13 01:17:01 +0000

    fix glb button. filesize defined in constants

--- src/constants.js
+++ src/constants.js
@@ -18,8 +18,9 @@ export const ROUTES = {
   LOAD: { exact: false, path: '/load', component: null }, // TODO: check if this is being used?
 }
 
-export const MINT_MAX_LIMIT = 10000
 export const MINT_MIN_LIMIT = 1
+export const MINT_MAX_LIMIT = 10000
+export const MINT_FILESIZE = 40
 
 export const MIMETYPE = {
   BMP: 'image/bmp',
@@ -32,7 +33,10 @@ export const MIMETYPE = {
   MP4: 'video/mp4',
   OGG: 'video/ogg',
   QUICKTIME: 'video/quicktime',
-  GLTF: 'model/gltf-binary',
-  GLB: 'model/gltf+json',
+  GLB: 'model/gltf-binary',
+  GLTF: 'model/gltf+json',
 }
+
 export const ALLOWED_MIMETYPES = Object.keys(MIMETYPE).map((k) => MIMETYPE[k])
+
+export const ALLOWED_FILETYPES = Object.keys(MIMETYPE)
