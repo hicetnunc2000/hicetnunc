add02109d459a263912e06af6d2a09b139435c92
Göran Sandström <v3qt0r@gmail.com>
2021-03-15 08:25:03 +0100

    Update api.js

--- src/data/api.js
+++ src/data/api.js
@@ -11,9 +11,6 @@ export const GetFeed = async ({ counter }) => {
     axios
       .post(process.env.REACT_APP_FEED, {
         counter: counter,
-      })
-      .catch(() => {
-        return { data: { result: [] } }
       }),
     axios.get(process.env.REACT_APP_BLOCKLIST_OBJKT).catch(() => {
       return { data: [] }
@@ -39,10 +36,6 @@ export const GetFeed = async ({ counter }) => {
         original,
       }
     })
-    .catch((e) => {
-      console.error(e)
-      return []
-    })
 }
 
 /**
@@ -55,7 +48,6 @@ export const GethDAOFeed = async ({ counter }) => {
       .then((res) => {
         resolve(res.data.result)
       })
-      .catch((e) => reject(e))
   })
 }
 
