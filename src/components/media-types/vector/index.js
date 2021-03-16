fed15e87878adfdc623d400aa0acd9d4a06f169b
André Venâncio <info@andrevenancio.com>
2021-03-16 23:42:24 +0000

    wip

--- src/components/media-types/vector/index.js
+++ src/components/media-types/vector/index.js
@@ -2,9 +2,14 @@ import React from 'react'
 import styles from './index.module.scss'
 
 export const VectorComponent = ({ src }) => {
+  console.log('vector', src)
   return (
     <div className={styles.container}>
-      <object className={styles.object} data={src} type="image/svg+xml"><p>error: use a more modern browser</p></object>
+      <iframe
+        title={src}
+        src={`https://hicetnunc.tiiny.site?src=${src}`}
+        sandbox="allow-same-origin allow-scripts"
+      />
     </div>
   )
 }
