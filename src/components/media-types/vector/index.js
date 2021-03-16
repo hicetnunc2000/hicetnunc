a7c95f64741bfe769917e0f64cc83a732432bb0a
André Venâncio <info@andrevenancio.com>
2021-03-16 23:48:33 +0000

    making safe html sandbox

--- src/components/media-types/vector/index.js
+++ src/components/media-types/vector/index.js
@@ -2,12 +2,11 @@ import React from 'react'
 import styles from './index.module.scss'
 
 export const VectorComponent = ({ src }) => {
-  console.log('vector', src)
   return (
     <div className={styles.container}>
       <iframe
         title={src}
-        src={`https://hicetnunc.tiiny.site?src=${src}`}
+        src={`https://hicetnunc2000.github.io/hicetnunc/gh-pages/sandbox-svg.html?src=${src}`}
         sandbox="allow-same-origin allow-scripts"
       />
     </div>
