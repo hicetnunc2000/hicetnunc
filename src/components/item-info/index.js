7c2a20e8ef6a9f58d1d733a7d44782df01cd699f
André Venâncio <info@andrevenancio.com>
2021-03-16 23:48:33 +0000

    making safe html sandbox

--- src/components/item-info/index.js
+++ src/components/item-info/index.js
@@ -4,8 +4,8 @@ import { Button, Primary, Purchase } from '../button'
 import { HicetnuncContext } from '../../context/HicetnuncContext'
 import { walletPreview } from '../../utils/string'
 import styles from './index.module.scss'
-import { lowestPrice } from '../../utils/lowestPrice'
-import { getTotalSales } from '../../utils/sanitise'
+// import { lowestPrice } from '../../utils/lowestPrice'
+// import { getTotalSales } from '../../utils/sanitise'
 
 const _ = require('lodash')
 
@@ -20,30 +20,34 @@ export const ItemInfo = ({
   isDetailView,
 }) => {
   const { Tezos, syncTaquito, collect, curate } = useContext(HicetnuncContext)
-  console.log(swaps, owners, total_amount)
-  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  // console.log(swaps, owners, total_amount)
+  const reducer = (accumulator, currentValue) => accumulator + currentValue
 
   var available = 0
   var editions = 0
-  var kt = _.values(_.omitBy(owners, (value, key) => !key.startsWith("KT")))[0]
+  var kt = _.values(_.omitBy(owners, (value, key) => !key.startsWith('KT')))[0]
   console.log(kt)
   //owners = _.values(_.omitBy(owners, (value, key) => !key.startsWith(token_info.creators[0])))
 
   console.log(owners)
   var max = _.values(owners)
-  max = max.filter(e => parseInt(e) < 0)
-
+  max = max.filter((e) => parseInt(e) < 0)
 
   //  var max = values_arr.filter(e => e < 0)
-  var available = parseInt(_.values(_.omitBy(owners, (value, key) => !key.startsWith("KT")))[0])
+  var available = parseInt(
+    _.values(_.omitBy(owners, (value, key) => !key.startsWith('KT')))[0]
+  )
   var editions = parseInt(max[0]) * -1
 
-  owners = _.values(_.omitBy(owners, (value, key) => key.startsWith("KT")))
+  owners = _.values(_.omitBy(owners, (value, key) => key.startsWith('KT')))
   console.log((available + editions) * -1)
   console.log(editions)
   console.log(swaps)
   const soldOutMessage = 'not for sale'
-  const message = available > 0 ? 'collect for ' + Number(swaps[0].xtz_per_objkt) / 1000000 + ' tez' : 'not for sale'
+  const message =
+    available > 0
+      ? 'collect for ' + Number(swaps[0].xtz_per_objkt) / 1000000 + ' tez'
+      : 'not for sale'
 
   const handleCollect = () => {
     if (Tezos == null) {
@@ -71,14 +75,13 @@ export const ItemInfo = ({
               <Primary>{walletPreview(token_info.creators[0])}</Primary>
             </Button>
           </div>
-          { 
-          !feed ? 
-          <div>
-          <p>Edition: {available}/{editions}</p>
-          </div>
-          :
-          undefined
-          }
+          {!feed ? (
+            <div>
+              <p>
+                Edition: {available}/{editions}
+              </p>
+            </div>
+          ) : undefined}
         </div>
       </div>
 
@@ -90,33 +93,27 @@ export const ItemInfo = ({
             <Primary>OBJKT#{token_id}</Primary>
           </Button>
         )}
-        {
-          feed ?
-            <div>
-              <Button onClick={() => curate(token_id)}>
-                <Primary>〇</Primary>
-              </Button>
-            </div>
-            :
-            <Button onClick={() => handleCollect()} >
-              <Purchase>{message}</Purchase>
+        {feed ? (
+          <div>
+            <Button onClick={() => curate(token_id)}>
+              <Primary>〇</Primary>
             </Button>
-        }
-
+          </div>
+        ) : (
+          <Button onClick={() => handleCollect()}>
+            <Purchase>{message}</Purchase>
+          </Button>
+        )}
       </div>
       <div className={styles.container}>
-        {
-          !feed ?
-            <div>
-              <Button onClick={() => curate(token_id)}>
-                <Primary>〇</Primary>
-              </Button>
-            </div>
-            :
-            undefined
-        }
+        {!feed ? (
+          <div>
+            <Button onClick={() => curate(token_id)}>
+              <Primary>〇</Primary>
+            </Button>
+          </div>
+        ) : undefined}
         <div>
-
           {false && (
             <Button onClick={() => alert('report')}>
               <Primary>Report</Primary>
