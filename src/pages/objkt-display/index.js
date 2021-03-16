51a5220de0660b9f2c30ba36011b05cc32ccae57
hicetnunc2000 <180111973@aluno.unb.br>
2021-03-16 05:24:07 -0300

    latest

--- src/pages/objkt-display/index.js
+++ src/pages/objkt-display/index.js
@@ -29,6 +29,8 @@ export const ObjktDisplay = () => {
   useEffect(() => {
     GetOBJKT({ id }).then((objkt) => {
       // add sanitation here
+      console.log(objkt)
+      objkt.feed = false
       setNFT(objkt)
       setLoading(false)
     })
