1ddfa21302ca52b56e778144c13b7ee3937e3206
André Venâncio <info@andrevenancio.com>
2021-03-18 11:18:06 +0000

    svg renderer

--- src/components/media-types/vector/index.js
+++ src/components/media-types/vector/index.js
@@ -5,8 +5,8 @@ export const VectorComponent = ({ src }) => {
   return (
     <div className={styles.container}>
       <iframe
-        title={src}
-        src={`https://hicetnunc2000.github.io/hicetnunc/gh-pages/sandbox-svg.html?src=${src}`}
+        title="hic et nunc SVG renderer"
+        src={src}
         sandbox="allow-same-origin allow-scripts"
         scrolling="no"
       />
