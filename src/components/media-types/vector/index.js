27dbad97415e8836d966f1d8b4cb475471fae535
André Venâncio <info@andrevenancio.com>
2021-03-19 11:36:09 +0000

    temp-wip-dont-push

--- src/components/media-types/vector/index.js
+++ src/components/media-types/vector/index.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import classnames from 'classnames'
+import { HicetnuncContext } from '../../../context/HicetnuncContext'
 import styles from './index.module.scss'
 
 export const VectorComponent = ({ src, interactive }) => {
+  const context = useContext(HicetnuncContext)
   const classes = classnames({
     [styles.container]: true,
     [styles.interactive]: interactive,
   })
+
+  console.log(context)
+  let author = 'no-author-logged'
+  let viewer = 'no-viewer-logged'
+
   return (
     <div className={classes}>
       <iframe
         title="hic et nunc SVG renderer"
-        src={src}
+        src={`${src}?author=${author}&viewer=${viewer}`}
         sandbox="allow-scripts"
         scrolling="no"
       />
