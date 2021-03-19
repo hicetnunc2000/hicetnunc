f3b92f73da78561e7b9d0178c7846b6d01f13a66
Daniel Massey <pichiste@gmail.com>
2021-03-19 16:18:28 +0100

    injecting CSP meta tag into both HTML preview and file upload to prevent external sources

--- src/components/media-types/html/index.js
+++ src/components/media-types/html/index.js
@@ -1,17 +1,21 @@
 import React from 'react'
+import { injectCSPMetaTagIntoDataURI } from '../../../utils/html'
 import classnames from 'classnames'
 import styles from './index.module.scss'
 
-export const HTMLComponent = ({ src, interactive }) => {
+export const HTMLComponent = ({ src, interactive, preview }) => {
   const classes = classnames({
     [styles.container]: true,
     [styles.interactive]: interactive,
   })
+
+  const safeSrc = preview ? injectCSPMetaTagIntoDataURI(src) : src
+
   return (
     <div className={classes}>
       <iframe
         title="hic et nunc HTML renderer"
-        src={src}
+        src={safeSrc}
         sandbox="allow-scripts"
         scrolling="no"
       />
