2c3421eefcb63f22f9883db47a8ffd308d2941ef
Daniel Massey <hi@daniel-massey.com>
2021-03-23 17:48:04 +0100

    WIP ipfs

--- src/data/ipfs.js
+++ src/data/ipfs.js
@@ -1,7 +1,8 @@
 const createClient = require('ipfs-http-client')
 const Buffer = require('buffer').Buffer
 const { v4: uuidv4 } = require('uuid')
-const { post } = require('axios')
+const axios = require('axios')
+const FormDataCustom = require('form-data')
 
 export const prepareFile = async ({
   name,
@@ -43,50 +44,51 @@ export const prepareDirectory = async ({
   files,
 }) => {
   // upload files
-  const hash = await uploadFilesToDirectory(files)
-  const cid = `ipfs://${hash}`
+  await uploadFilesToDirectory(files)
 
-  // create and upload meta file
-  const ipfs = createClient('https://ipfs.infura.io:5001')
-  const result = await ipfs.files.add(
-    Buffer.from(
-      JSON.stringify({
-        name,
-        description,
-        tags: tags.replace(/\s/g, '').split(','),
-        symbol: 'OBJKT',
-        artifactUri: cid,
-        creators: [address],
-        formats: [{ uri: cid, mimeType: 'application/x-directory' }],
-        thumbnailUri: 'ipfs://QmNrhZHUaEqxhyLfqoq1mtHSipkWHeT31LNHb1QEbDHgnc',
-        decimals: 0,
-        isBooleanAmount: false,
-        shouldPreferSymbol: false,
-      })
-    )
-  )
+  // const cid = `ipfs://${hash}`
 
-  return result
+  // // create and upload meta file
+  // const ipfs = createClient('https://ipfs.infura.io:5001')
+  // const result = await ipfs.files.add(
+  //   Buffer.from(
+  //     JSON.stringify({
+  //       name,
+  //       description,
+  //       tags: tags.replace(/\s/g, '').split(','),
+  //       symbol: 'OBJKT',
+  //       artifactUri: cid,
+  //       creators: [address],
+  //       formats: [{ uri: cid, mimeType: 'application/x-directory' }],
+  //       thumbnailUri: 'ipfs://QmNrhZHUaEqxhyLfqoq1mtHSipkWHeT31LNHb1QEbDHgnc',
+  //       decimals: 0,
+  //       isBooleanAmount: false,
+  //       shouldPreferSymbol: false,
+  //     })
+  //   )
+  // )
+
+  // return result
 }
 
 async function uploadFilesToDirectory (files) {
-  const formData = new FormData()
-
   console.log('Upload files to IPFS')
   console.log(files)
 
+  console.log('FormDataCustom', FormDataCustom)
+  const form = new FormDataCustom()
+
   files.forEach(file => {
-    formData.append('file', file.blob, {
-      filepath: file.path
+    form.append('file', file.blob, {
+      filepath: file.path,
+      contentType: file.blob.type
     })
   })
 
   const endpoint = 'https://ipfs.infura.io:5001/api/v0/add?pin=true&recursive=true&wrap-with-directory=true'
-  const res = await post(endpoint, formData, {
-    headers: {
-      'Content-Type': 'multipart/form-data'
-    }
+  const res = await axios.post(endpoint, form, {
+    headers: form.getHeaders()
   })
 
-  return res.data.Hash
+  console.log(res)
 }
