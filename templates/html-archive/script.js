c0037acdec48ec74f67e15944d0c9dcf6f6f333e
André Venâncio <info@andrevenancio.com>
2021-03-24 15:04:41 +0000

    finalising HTML

--- templates/html-archive/script.js
+++
@@ -1,3 +0,0 @@
-const creator = new URLSearchParams(window.location.search).get('creator')
-const viewer = new URLSearchParams(window.location.search).get('viewer')
-console.log('minted by', creator, 'viewer', viewer)
