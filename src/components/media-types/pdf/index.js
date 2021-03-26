0193e48fafb1e23c72c1af4dd68427eab097cafa
Clement Renaud <clement@clementrenaud.com>
2021-03-26 14:31:07 +0100

    pdf: view in iframe

--- src/components/media-types/pdf/index.js
+++ src/components/media-types/pdf/index.js
@@ -4,7 +4,13 @@ import styles from './index.module.scss'
 export const PdfComponent = ({  }) => {
     return (
         <div className={styles.container}>
-            <div className={styles.square}>{mimeType} NOT SUPPORTED (yet)</div>
+            <div className={styles.square}>        
+                <iframe
+                    title="hic et nunc SVG renderer"
+                    src={`${src}?`}
+                    scrolling="no"
+                />
+            </div>
         </div>
     )
 }
