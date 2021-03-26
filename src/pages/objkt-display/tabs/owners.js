92ab7c27465b8a0efb98e08fb42df6c2834b0ded
crzypatchwork <costabraba93@gmail.com>
2021-03-25 23:58:55 -0300

    sync

--- src/pages/objkt-display/tabs/owners.js
+++ src/pages/objkt-display/tabs/owners.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Container, Padding } from '../../../components/layout'
 import { OwnerList } from '../../../components/owner-list'
+const _ = require('lodash')
 
-export const Owners = ({ owners }) => {
+export const Owners = ({ owners, swaps }) => {
+  console.log (owners, swaps)
+  console.log(_.merge(owners, swaps))
   const filtered =
     (owners &&
       Object.keys(owners)
