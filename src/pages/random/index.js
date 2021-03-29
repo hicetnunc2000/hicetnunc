be8428bc773a4fb0fd8869a363df8a4d6188a3e2
terry.may <terry.may@gmail.com>
2021-03-29 10:18:56 -0400

    Slide Show Mode for the Random page  * Added slide-show mode to random feed page  * Added Button to switch feed from scrolling to slide show    ** I'm not married to button iconography or placement, looking for suggestions  * Changed logic to hide 'load more' and 'minting' messages when slide show is running  * I used an inline style because I am still learning how css works here
    Future Work
     * css fade in and out between changes
     * allow users to set the change interval
     * pause interval when a video is playing it's first loop
     * clean full screen mode

--- src/pages/random/index.js
+++ src/pages/random/index.js
@@ -1,70 +1,123 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from 'react'
-import { GetFeed, GethDAOFeed, GetRandomFeed } from '../../data/api'
-import { Page, Container, Padding } from '../../components/layout'
-import { FeedItem } from '../../components/feed-item'
-import { Loading } from '../../components/loading'
-import { Button, Primary } from '../../components/button'
-import styles from './index.module.scss'
+import React, { useEffect, useState } from "react";
+import { GetFeed, GethDAOFeed, GetRandomFeed } from "../../data/api";
+import { useInterval } from "../../hooks/useInterval";
+import { Page, Container, Padding } from "../../components/layout";
+import { FeedItem } from "../../components/feed-item";
+import { Loading } from "../../components/loading";
+import { Button, Primary } from "../../components/button";
+import styles from "./index.module.scss";
 
 const customFloor = function (value, roundTo) {
-  return Math.floor(value / roundTo) * roundTo
-}
+  return Math.floor(value / roundTo) * roundTo;
+};
 
-const ONE_MINUTE_MILLIS = 60 * 1000
+const ONE_MINUTE_MILLIS = 60 * 1000;
+const SLIDE_SHOW_DELAY = 15 * 1000;
 
 export const Random = () => {
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(false)
-  const [feedType, setFeedType] = useState(1)
-  const [items, setItems] = useState([])
-  const [count, setCount] = useState(0)
-  const [hasMore, setHasMore] = useState(true)
-  const startTime = customFloor(Date.now(), ONE_MINUTE_MILLIS)
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [feedType, setFeedType] = useState(1);
+  const [items, setItems] = useState([]);
+  const [count, setCount] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+  const [slideShowIndex, setSlideShowIndex] = useState(0);
+  const [slideShowisRunning, setSlideShowisRunning] = useState(false);
+  const startTime = customFloor(Date.now(), ONE_MINUTE_MILLIS);
 
   const loadMore = () => {
-    setCount(count + 1)
-  }
+    setCount(count + 1);
+  };
 
   useEffect(() => {
     if (error) {
-      console.log('returning on error')
-      return
+      console.log("returning on error");
+      return;
     }
 
     if (feedType === 0) {
-      console.log('hDAO feed')
-
+      console.log("hDAO feed");
     } else {
-      console.log('latest feed')
+      console.log("latest feed");
 
-      setLoading(true)
+      setLoading(true);
       // api
       GetRandomFeed({ counter: count })
-        .then(result => {
+        .then((result) => {
           // filtered isn't guaranteed to always be 10. if we're filtering they might be less.
-          console.log(result)
-          const next = items.concat(result)
-          setItems(next)
-          setLoading(false)
+          console.log(result);
+          const next = items.concat(result);
+          setItems(next);
+          setLoading(false);
           // if original returns less than 30, then there's no more data coming from API
           if (result.length < 30) {
-            setHasMore(false)
+            setHasMore(false);
           }
         })
         .catch((e) => {
-          setError(true)
-          setLoading(false)
-        })
+          setError(true);
+          setLoading(false);
+        });
     }
-  }, [count, feedType])
+  }, [count, feedType]);
 
   const toggleFeed = (index) => {
-    setFeedType(index)
-    setCount(0)
-    setItems([])
-    setHasMore(true)
-  }
+    setFeedType(index);
+    setCount(0);
+    setItems([]);
+    setHasMore(true);
+  };
+
+  const getBottomStatus = () => {
+    if (slideShowisRunning) {
+      return <></>;
+    }
+    return (
+      <>
+        {hasMore ? (
+          <Button onClick={loadMore}>
+            <Primary>
+              <strong>Load More</strong>
+            </Primary>
+          </Button>
+        ) : (
+          <p>
+            mint mint mint{" "}
+            <span role="img" aria-labelledby={"Sparkles emoji"}>
+              ✨
+            </span>
+          </p>
+        )}
+      </>
+    );
+  };
+
+  //for slide show mode
+  const onNextObjkt = () => {
+    if (slideShowIndex < items.length - 1) {
+      setSlideShowIndex(slideShowIndex + 1);
+    } else {
+      setSlideShowisRunning(false);
+      setSlideShowIndex(0);
+      loadMore();
+      setCount(count + 1);
+    }
+  };
+
+  const getFeedDisplayIcon = () =>
+    items.length > 0 ? (
+      <div
+        style={{ paddingLeft: 200, cursor: "pointer" }}
+        onClick={() => setSlideShowisRunning(!slideShowisRunning)}
+      >
+        {slideShowisRunning ? "〓" : "⿴"}
+      </div>
+    ) : (
+      <div></div>
+    );
+
+  useInterval(onNextObjkt, slideShowisRunning ? SLIDE_SHOW_DELAY : null);
 
   return (
     <Page>
@@ -81,10 +134,18 @@ export const Random = () => {
         </div>
       )}
 
-      {items.map((item, index) => (
-        <FeedItem key={`${item.token_id}-${index}`} {...item} />
-      ))}
+      {getFeedDisplayIcon()}
 
+      {slideShowisRunning ? (
+        <FeedItem
+          key={`${items[slideShowIndex].token_id}-${slideShowIndex}`}
+          {...items[slideShowIndex]}
+        />
+      ) : (
+        items.map((item, index) => (
+          <FeedItem key={`${item.token_id}-${index}`} {...item} />
+        ))
+      )}
       {loading ? (
         <Container>
           <Padding>
@@ -93,24 +154,9 @@ export const Random = () => {
         </Container>
       ) : (
         <Container>
-          <Padding>
-            {hasMore ? (
-              <Button onClick={loadMore}>
-                <Primary>
-                  <strong>Load More</strong>
-                </Primary>
-              </Button>
-            ) : (
-              <p>
-                mint mint mint{' '}
-                <span role="img" aria-labelledby={'Sparkles emoji'}>
-                  ✨
-                </span>
-              </p>
-            )}
-          </Padding>
+          <Padding>{getBottomStatus()}</Padding>
         </Container>
       )}
     </Page>
-  )
-}
+  );
+};
