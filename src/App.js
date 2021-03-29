024ff37b325edb5d70a23027cee595a68b3d4e0f
André Venâncio <info@andrevenancio.com>
2021-03-29 17:05:45 +0100

    revert footer back to main, instead of header

--- src/App.js
+++ src/App.js
@@ -56,6 +56,7 @@ const App = () => {
         <Route path="/objkt/:id" component={ObjktDisplay} />
         <Route path="/load" component={Loading} />
       </Switch>
+      <Footer />
     </HicetnuncContextProvider>
   )
 }
