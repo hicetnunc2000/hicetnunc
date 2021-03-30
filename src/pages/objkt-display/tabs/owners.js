4c8459d2d698a9534e724916ea227b1198ab2a75
crzypatchwork <costabraba93@gmail.com>
2021-03-30 14:33:41 -0300

    fix

--- src/pages/objkt-display/tabs/owners.js
+++ src/pages/objkt-display/tabs/owners.js
@@ -2,17 +2,18 @@ import React, { useContext } from 'react'
 import { Container, Padding } from '../../../components/layout'
 import { OwnerList } from '../../../components/owner-list'
 import { HicetnuncContext } from '../../../context/HicetnuncContext'
-import { OwnerSwaps } from '../../../components/owner-swaps'
+import { Button, Primary, Purchase } from '../../../components/button'
+import { walletPreview } from '../../../utils/string'
+
 
 const _ = require('lodash')
 
 export const Owners = ({ owners, swaps }) => {
-  const { syncTaquito, collect, acc, getAccount, cancel } = useContext(
-    HicetnuncContext
-  )
+  const { Tezos, syncTaquito, collect, curate, acc, getAccount, cancel } = useContext(HicetnuncContext)
   console.log(owners, swaps)
   console.log(_.merge(owners, swaps))
 
+  const s = _.minBy(swaps, 'xtz_per_objkt')
   const filtered =
     (owners &&
       Object.keys(owners)
@@ -22,6 +23,7 @@ export const Owners = ({ owners, swaps }) => {
     []
 
   const handleCollect = (swap_id, xtz_per_objkt) => {
+
     if (acc == null) {
       syncTaquito()
       getAccount()
@@ -31,19 +33,44 @@ export const Owners = ({ owners, swaps }) => {
   }
   return (
     <>
-      {swaps.length > 0 && (
-        <Container>
-          <Padding>
-            <OwnerSwaps
-              swaps={swaps}
-              handleCollect={handleCollect}
-              acc={acc}
-              cancel={cancel}
-            />
-          </Padding>
-        </Container>
-      )}
-      {filtered.length !== 0 && (
+      {
+        swaps.length > 0 ?
+          <Container>
+            <Padding>
+              {
+                swaps.map(e => {
+
+                  return (
+                    <div style={{ marginBottom: `10px` }}>
+                      <span>{e.objkt_amount}x <a style={{ color: '#000', '&:hover': { color: 'white', textDecoration: 'underline' } }} href={'/tz/' + e.issuer}>{walletPreview(e.issuer)}</a></span>
+                      <span style={{ float: 'right' }}>
+                        <Button onClick={() => handleCollect(e.swap_id, e.xtz_per_objkt)}>
+                          <Purchase>collect for {parseFloat(e.xtz_per_objkt / 1000000)} tez</Purchase>
+                        </Button>
+                      </span>
+                      {
+                        e.issuer == (acc !== undefined ? acc.address : '') ?
+                          <span style={{ float: 'right' }}>
+                            <Button onClick={() => cancel(e.swap_id)}>
+                              <Purchase>cancel</Purchase>
+                            </Button>
+                          </span>
+                          :
+                          undefined
+                      }
+                      {/* cancel */}
+                    </div>
+                  )
+                })
+              }
+            </Padding>
+          </Container>
+          :
+          undefined
+      }
+      {filtered.length === 0 ? (
+        undefined
+      ) : (
         <Container>
           <Padding>
             <OwnerList owners={filtered} />
