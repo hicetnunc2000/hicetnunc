c606c1bf6f60d451123cebc5ba3748f43e66ae48
Nikita Dudnik <i@awayfrompeople.com>
2021-04-03 10:34:00 +0200

    Add title to the audio OBJKTs previews

--- src/components/media-types/audio/index.js
+++ src/components/media-types/audio/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Padding } from '../../layout/padding'
 import styles from './index.module.scss'
 
-export const AudioComponent = ({ src }) => (
-  <div className={styles.container}>
-    <audio src={src} controls />
-  </div>
+export const AudioComponent = ({ src, token_info }) => (
+  <>
+    <Padding>{token_info.name}</Padding>
+    <div className={styles.container}>
+      <audio title={token_info.name} src={src} controls />
+    </div>
+  </>
 )
