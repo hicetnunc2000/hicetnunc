63dea3de48e9b45fe272631d9571898734ef4191
André Venâncio <info@andrevenancio.com>
2021-04-05 20:53:57 +0100

    removing burn wallet from collectors. changed file from Owners to Collecters to reflect UI

--- src/utils/string.js
+++ src/utils/string.js
@@ -1,10 +1,10 @@
 export const walletPreview = (wallet) => {
   try {
-  return `${wallet.slice(0, 5)}...${wallet.slice(
-    wallet.length - 5,
-    wallet.length
-  )}`
+    return `${wallet.slice(0, 5)}...${wallet.slice(
+      wallet.length - 5,
+      wallet.length
+    )}`
   } catch (e) {
     return ''
   }
-} 
+}
