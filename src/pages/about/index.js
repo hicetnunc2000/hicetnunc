b9ba6e9d7fae1605e4b27ea0b000ed8c5ad66e43
André Venâncio <info@andrevenancio.com>
2021-04-05 21:01:31 +0100

    implement page title on about

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Container, Padding } from '../../components/layout'
+import { Page, Container, Padding } from '../../components/layout'
 import { Button, Primary } from '../../components/button'
 import { HicetnuncContext } from '../../context/HicetnuncContext'
 import { getLanguage } from '../../constants'
@@ -22,7 +22,7 @@ export class About extends Component {
 
   render() {
     return (
-      <>
+      <Page title="About">
         <Container>
           <Padding>
             <strong>hic et nunc</strong>
@@ -98,7 +98,7 @@ export class About extends Component {
             </div>
           </Padding>
         </Container>
-      </>
+      </Page>
     )
   }
 }
