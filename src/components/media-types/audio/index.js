0fe2ac6079fe5ce677cb07e5e7d90fc55e85967b
André Venâncio <info@andrevenancio.com>
2021-04-07 13:55:59 +0100

    remove title

--- src/components/media-types/audio/index.js
+++ src/components/media-types/audio/index.js
@@ -4,7 +4,6 @@ import styles from './index.module.scss'
 
 export const AudioComponent = ({ src, token_info }) => (
   <>
-    <Padding>{token_info.name}</Padding>
     <div className={styles.container}>
       <audio title={token_info.name} src={src} controls />
     </div>
