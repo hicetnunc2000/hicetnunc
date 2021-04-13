4e8ebcd82ec934ae01458426a4635eb2e72c34ca
Andre Venancio <info@andrevenancio.com>
2021-04-13 23:55:49 +0100

    remove nft

--- src/components/media-types/audio/index.js
+++ src/components/media-types/audio/index.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { PauseIcon, PlayIcon } from './icons'
+import { Visualiser } from './visualiser'
 import styles from './styles.module.scss'
 
-export const AudioComponent = ({ src, token_info }) => (
-  <>
-    <div className={styles.container}>
-      <audio title={token_info.name} src={src} controls />
-    </div>
-  </>
-)
+export const AudioComponent = ({ src, token_info }) => {
+  const [userTouched, setUserTouched] = useState(false)
+  const [play, setPlay] = useState(false)
+
+  const togglePlay = () => {
+    console.log('toggle play')
+    userTouched(true)
+    setPlay(!play)
+  }
+
+  useEffect(() => {
+    // const audio = new Audio()
+    // audio.src = src
+    // audio.controls = false
+    // audio.loop = true
+    // audio.autoplay = true
+    // audio.crossOrigin = 'anonymous'
+  }, [userTouched])
+
+  return (
+    <>
+      <div className={styles.container}>
+        {false && <img src="/test.png" alt="album cover" />}
+        {true && <audio title={token_info.name} src={src} controls />}
+        {false && <Visualiser />}
+        {false && (
+          <div className={styles.icons} onClick={togglePlay}>
+            {play ? <PauseIcon /> : <PlayIcon />}
+          </div>
+        )}
+      </div>
+    </>
+  )
+}
