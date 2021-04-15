2fa0703c7cf91e536fad2f53ab1990c8fd0502a3
Daniel Massey <hi@daniel-massey.com>
2021-04-15 19:13:24 +0200

    implemented lazy loadign on galleries

--- src/pages/gallery-detail/item/index.js
+++ src/pages/gallery-detail/item/index.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
+import { useInView } from 'react-intersection-observer'
 import { GetOBJKT } from '../../../data/api'
 import { renderMediaType } from '../../../components/media-types'
 import { Loading } from '../../../components/loading'
@@ -9,6 +10,10 @@ import styles from './styles.module.scss'
 
 export const Item = ({ objkt }) => {
   const [data, setData] = useState()
+  const { ref, inView } = useInView({
+    rootMargin: '0px 0px 50% 0px',
+  })
+  const shown = useRef(false)
 
   useEffect(() => {
     GetOBJKT({ id: objkt })
@@ -21,17 +26,27 @@ export const Item = ({ objkt }) => {
       .catch((e) => console.log('error loading', objkt))
   }, [objkt])
 
+  if (inView && !shown.current) {
+    shown.current = true
+  }
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} ref={ref}>
       <div>
         {data ? (
           <Button to={`${PATH.OBJKT}/${objkt}`} key={objkt}>
-            <div className={styles.image}>
-              {renderMediaType({
-                ...data,
-              })}
-              <div className={styles.number}>OBJKT#{objkt}</div>
-            </div>
+            {inView || shown ? (
+              <div className={styles.image}>
+                {renderMediaType({
+                  ...data,
+                  shown: shown.current,
+                  inView,
+                })}
+                <div className={styles.number}>OBJKT#{objkt}</div>
+              </div>
+            ) : (
+              <div className={styles.image}>OFFSCREEN</div>
+            )}
           </Button>
         ) : (
           <div>
