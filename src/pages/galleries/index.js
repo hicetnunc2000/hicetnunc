faba6b9fa1fba094f48bf68d23e22ae41acf34b8
Andre Venancio <info@andrevenancio.com>
2021-04-19 01:24:39 +0100

    adding galleries page

---
+++ src/pages/galleries/index.js
@@ -0,0 +1,81 @@
+import { useEffect, useState } from 'react'
+import { Button } from '../../components/button'
+import { Page, Container, Padding } from '../../components/layout'
+import { Loading } from '../../components/loading'
+import { GetOBJKT } from '../../data/api'
+import { renderMediaType } from '../../components/media-types'
+import { PATH } from '../../constants'
+import { ResponsiveMasonry } from '../../components/responsive-masonry'
+import styles from './styles.module.scss'
+
+export const Galleries = () => {
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState(false)
+
+  useEffect(() => {
+    // loads gallery to check endpoint file
+    fetch('/galleries/galleries.json')
+      .then((e) => e.json())
+      .then(async (galleries) => {
+        const g = []
+        let c = 0
+        galleries.forEach(async (element) => {
+          await GetOBJKT({ id: element.thumbnail }).then((e) => {
+            const found = galleries.find((e) => e.uid === element.uid)
+            g.push(Object.assign({}, found, e))
+            c++
+
+            if (c === 3) {
+              setData(g)
+              setLoading(false)
+            }
+          })
+        })
+      })
+
+    return () => {
+      document.body.style = {}
+    }
+  }, [])
+  return (
+    <Page title="Galleries">
+      {loading ? (
+        <Container>
+          <Padding>
+            <Loading />
+          </Padding>
+        </Container>
+      ) : (
+        <Container xlarge>
+          <Padding>
+            <ResponsiveMasonry>
+              {data.map((e) => {
+                const { token_info } = e
+                const { mimeType, uri } = token_info.formats[0]
+
+                return (
+                  <Button key={e.uid} to={`${PATH.GALLERY}/${e.uid}`}>
+                    <div className={styles.item}>
+                      <div
+                        style={{ pointerEvents: 'none' }}
+                        className={styles.image}
+                      >
+                        {renderMediaType({
+                          uri: uri.split('//')[1],
+                          mimeType,
+                          metadata: e,
+                          interactive: false,
+                        })}
+                        <div className={styles.number}>{e.name}</div>
+                      </div>
+                    </div>
+                  </Button>
+                )
+              })}
+            </ResponsiveMasonry>
+          </Padding>
+        </Container>
+      )}
+    </Page>
+  )
+}
