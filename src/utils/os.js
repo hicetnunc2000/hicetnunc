ed4a7bacf28d033ef5f33c16dfc7edaec327dfbe
Daniel Massey <hi@daniel-massey.com>
2021-04-19 19:19:28 +0200

    moved iOS check to utils

---
+++ src/utils/os.js
@@ -0,0 +1,11 @@
+export const iOS =
+  [
+    'iPad Simulator',
+    'iPhone Simulator',
+    'iPod Simulator',
+    'iPad',
+    'iPhone',
+    'iPod',
+  ].includes(navigator.platform) ||
+  // iPad on iOS 13 detection
+  (navigator.userAgent.includes('Mac') && 'ontouchend' in document)
