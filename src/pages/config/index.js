058584d09d7fc29378eefb7a891f6bfe05857b31
crzypatchwork <costabraba93@gmail.com>
2021-04-22 17:56:50 -0300

    scaffold

---
+++ src/pages/config/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { HicetnuncContext } from '../../context/HicetnuncContext'
+
+export class Config extends Component {
+    static contextType = HicetnuncContext
+
+    state = {}
+    
+    componentWillMount = () => console.log('oi')
+
+    // config alias
+    // config hDAO balance
+    
+    render() {
+        return(
+            <div>
+            </div>
+        )
+    }
+}
