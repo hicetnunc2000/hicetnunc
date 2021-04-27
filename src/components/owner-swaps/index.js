284937f9f9862dd7248a225da8e478f55a00c7f0
neelts <neeelts@gmail.com>
2021-04-27 15:34:25 +0300

    Remove collect button & add '(banned)' to the banned address.

--- src/components/owner-swaps/index.js
+++ src/components/owner-swaps/index.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import { Button, Primary, Purchase } from '../button'
 import { walletPreview } from '../../utils/string'
+import { getWalletBlockList } from "../../constants";
 import styles from './styles.module.scss'
 
 const sortByPrice = (a, b) => {
   return Number(a.xtz_per_objkt) - Number(b.xtz_per_objkt)
 }
 export const OwnerSwaps = ({ swaps, handleCollect, acc, cancel }) => {
+  const wblock = getWalletBlockList();
   return (
     <div className={styles.container}>
       {swaps.sort(sortByPrice).map((swap, index) => {
+        const banned = wblock.includes(swap.issuer);
         return (
           <div key={`${swap.swap_id}-${index}`} className={styles.swap}>
             <div className={styles.issuer}>
               {swap.objkt_amount} x&nbsp;
               <Button to={`/tz/${swap.issuer}`}>
-                <Primary>{walletPreview(swap.issuer)}</Primary>
+                <Primary>{walletPreview(swap.issuer)}{banned ? ' (banned)' : ''}</Primary>
               </Button>
             </div>
 
-            <div className={styles.buttons}>
+            {!banned && (
+                <div className={styles.buttons}>
               <Button
                 onClick={() => handleCollect(swap.swap_id, swap.xtz_per_objkt)}
               >
@@ -32,7 +36,7 @@ export const OwnerSwaps = ({ swaps, handleCollect, acc, cancel }) => {
                   <Purchase>cancel</Purchase>
                 </Button>
               )}
-            </div>
+            </div>)}
           </div>
         )
       })}
