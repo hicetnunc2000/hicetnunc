686e3c99ce5f9cb69c876eb692e4d759483dddd6
Andre Venancio <info@andrevenancio.com>
2021-05-02 23:00:09 +0100

    tags

--- src/components/tags/index.js
+++ src/components/tags/index.js
@@ -6,16 +6,15 @@ import styles from './styles.module.scss'
 export const Tags = ({ tags }) => {
   return (
     <div className={styles.container}>
-      {tags.map((tag, index) => {
-        return (
-          <Button
-            key={`tag${tag}${index}`}
-            to={`${PATH.TAGS}/${tag.toLowerCase()}`}
-          >
-            <div className={styles.tag}>{tag}</div>
-          </Button>
-        )
-      })}
+      {tags
+        .filter((e) => e === '')
+        .map((tag, index) => {
+          return (
+            <Button key={`tag${tag}${index}`} to={`${PATH.TAGS}/${tag}`}>
+              <div className={styles.tag}>{tag}</div>
+            </Button>
+          )
+        })}
     </div>
   )
 }
