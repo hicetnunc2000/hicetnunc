9b22893ce25dda25d169f9a6ce2e46b871b887e5
Fortune 707 <fortune600music@gmail.com>
2021-05-03 01:53:13 +0300

    preparation to add originate form

---
+++ src/pages/proxy-contract/tabs/originate.js
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react'
+import { Container, Padding } from '../../../components/layout'
+
+export const Originate = () => {
+  return (
+      <Container>
+        <Padding>
+            TODO: Originate form
+        </Padding>
+      </Container>
+  )
+}
+
