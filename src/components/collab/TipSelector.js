f56884ea5e6f076226cfe9b9c65ebb512664b730
Steve J <steve@raffworks.com>
2021-05-23 16:08:01 +0100

    Moved Tip selector to separate component

---
+++ src/components/collab/TipSelector.js
@@ -0,0 +1,20 @@
+import styles from './styles.module.scss'
+import { tipOptions } from './constants'
+import classNames from 'classnames'
+
+export const TipSelector = ({ tip, index, onUpdate }) => {
+    <div className={styles.tipSelect}>
+        {tipOptions.map(percentage => {
+            const selected = tip ? tip.percentage == percentage : false
+            const className = classNames(styles.btn, {
+                [styles.selected]: selected,
+            })
+
+            return (
+                <button className={className} key={`btn-${percentage}`} onClick={() => onUpdate(index, percentage)}>
+                    {percentage}%
+                </button>
+            )
+        })}
+    </div>
+}
