9b4c719cb290a0202b1fce435167562b7a91cd17
Jonathan Frascella <github@joff.org>
2021-05-23 01:16:21 +0100

    WIP - Add sharing url button

--- src/components/item-info/index.js
+++ src/components/item-info/index.js
@@ -61,6 +61,10 @@ export const ItemInfo = ({
     }
   }
 
+  const copySharingURL = (token_id) => {
+    navigator.clipboard.writeText('http://objkt.link/' + token_id);
+  }
+
   const curateOrClaim = (id, balance = 0) => {
     // if user is creator and there's hDAO balance
     if (acc && acc.address === token_info.creators[0] && balance > 0) {
@@ -138,6 +142,29 @@ export const ItemInfo = ({
         {isDetailView && (
           <div className={styles.container}>
             <p>OBJKT#{token_id}</p>
+            <span
+              data-position={'top'}
+              data-tooltip='copy sharing url'
+            >
+              <Button
+                onClick={() => copySharingURL(token_id)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  viewBox="0 0 16 16"
+                  style={{
+                    fill: 'var(--text-color)',
+                    stroke: 'transparent',
+                  }}
+                >
+                  <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z" />
+                  <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z" />
+                </svg>
+              </Button>
+            </span>
             <Button onClick={() => handleCollect()}>
               <Purchase>{message}</Purchase>
             </Button>
