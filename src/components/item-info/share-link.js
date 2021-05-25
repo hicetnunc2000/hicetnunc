f70f2c69f4fd4934778095eb782b0d399dfa2984
Noah Paessel <knowuh@gmail.com>
2021-05-25 17:16:53 -0400

    Use https for link service & rm link icon padding

--- src/components/item-info/share-link.js
+++ src/components/item-info/share-link.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button } from '../button'
 import styles from './styles.module.scss'
 
-const DefaultSharingUrl = 'http://objkt.link'
+const DefaultSharingUrl = 'https://objkt.link'
 
 const copySharingURL = (token_id, sharingUrl=DefaultSharingUrl) => {
   navigator.clipboard.writeText(`${sharingUrl}/${token_id}`);
@@ -17,8 +17,7 @@ const ShareIcon = () => {
       style={{
         fill: 'var(--text-color)',
         stroke: 'transparent',
-        height: '1em',
-        paddingLeft: '0.5em'
+        height: '1em'
       }}
     >
       <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z" />
