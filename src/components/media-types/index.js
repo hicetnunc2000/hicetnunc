b832dee61adb8e3d04434bd6ee6c05e4e7ef48e5
Daniel Massey <hi@daniel-massey.com>
2021-06-02 07:21:07 +0200

    fixing bug with displayUri for interactive objkts

--- src/components/media-types/index.js
+++ src/components/media-types/index.js
@@ -63,27 +63,23 @@ export const renderMediaType = (props) => {
       let displayUri = ''
       // old
       if (metadata && metadata.token_info && metadata.token_info.displayUri) {
-        displayUri = metadata.token_info.displayUri.replace(
-          'ipfs://',
-          IPFS
-        )
+        displayUri = metadata.token_info.displayUri.replace('ipfs://', IPFS)
       }
 
-      if (metadata && metadata.thumbnail_uri && metadata.thumbnail_uri !== 'ipfs://QmNrhZHUaEqxhyLfqoq1mtHSipkWHeT31LNHb1QEbDHgnc') {
-        displayUri = metadata.thumbnail_uri.replace(
-          'ipfs://',
-          IPFS
-        )
+      if (
+        metadata &&
+        metadata.display_uri &&
+        metadata.display_uri !==
+          'ipfs://QmNrhZHUaEqxhyLfqoq1mtHSipkWHeT31LNHb1QEbDHgnc'
+      ) {
+        displayUri = metadata.display_uri.replace('ipfs://', IPFS)
 
         if (metadata.display_uri) {
-          displayUri = metadata.thumbnail_uri.replace(
-            'ipfs://',
-            IPFS
-          )
+          displayUri = metadata.display_uri.replace('ipfs://', IPFS)
         }
       }
 
-      console.log(displayUri)
+      // console.log(displayUri)
 
       // new
       // if (metadata && metadata.display_uri) {
