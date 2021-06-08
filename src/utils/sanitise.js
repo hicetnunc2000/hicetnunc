451982268a9d8888a6105d41aeb48b2383b27c5d
Andre Venancio <info@andrevenancio.com>
2021-06-08 23:48:51 +0100

    work in progress

--- src/utils/sanitise.js
+++ src/utils/sanitise.js
@@ -96,3 +96,15 @@ export const formatBytes = (bytes, decimals = 2) => {
 
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
 }
+
+export const GetIPFSLink = (hash) => {
+  const CLOUDFLARE = 'https://cloudflare-ipfs.com/ipfs/'
+  // const PINATA = 'https://gateway.pinata.cloud/ipfs/'
+  const IPFS = 'https://ipfs.io/ipfs/'
+
+  function getInfuraUrl(hash) {
+    const cidv1 = new ipfsClient.CID(hash).toV1()
+    const subomain = cidv1.toString()
+    return `https://${subomain}.ipfs.infura-ipfs.io/`
+  }
+}
