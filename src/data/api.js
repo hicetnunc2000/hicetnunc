500385f76256dbf2833f93146953dcac92bc0626
Simon Bihel <simon.bihel@spruceid.com>
2021-06-09 10:35:09 +0100

    Integrate with tzprofiles
    Adds:
    - Twitter handle if not present in TzKT already; and
    - use alias and add logo for self-certified profile.

--- src/data/api.js
+++ src/data/api.js
@@ -143,10 +143,48 @@ export const GetTags = async ({ tag, counter }) => {
 }
 
 /**
- * Get User Metadata from tzkt.io
+ * Get User claims from their tzprofile
+ */
+const GetUserClaims = async (walletAddr) => {
+  return await axios.post('https://indexer.tzprofiles.com/v1/graphql', {
+    query: `query MyQuery { tzprofiles_by_pk(account: \"${walletAddr}\") { valid_claims } }`,
+    variables: null,
+    operationName: 'MyQuery',
+  })
+}
+
+/**
+ * Get User Metadata
  */
 export const GetUserMetadata = async (walletAddr) => {
-  return await axios.get(
+  let tzktData = await axios.get(
     `https://api.tzkt.io/v1/accounts/${walletAddr}/metadata`
   )
+
+  let tzpData = {}
+  try {
+    let claims = await GetUserClaims(walletAddr)
+    if (claims.data.data.tzprofiles_by_pk !== null)
+      for (const claim of claims.data.data.tzprofiles_by_pk.valid_claims) {
+        let claimJSON = JSON.parse(claim[1])
+        if (claimJSON.type.includes('TwitterVerification')) {
+          if (!tzktData.data || !tzktData.data.twitter ) {
+            tzpData['twitter'] = claimJSON.evidence.handle
+          }
+        } else if (claimJSON.type.includes('BasicProfile')) {
+          if (claimJSON.credentialSubject.alias !== "" && !(tzktData.data && tzktData.data.alias))
+            tzpData['alias'] = claimJSON.credentialSubject.alias
+          tzpData['tzprofile'] = walletAddr
+        }
+      }
+  } catch (e) {
+    console.error(e, e.stack);
+  }
+
+  if (tzktData.data !== '') {
+    tzktData.data = { ...tzpData, ...tzktData.data }
+  } else if (tzpData) {
+    tzktData.data = tzpData
+  }
+  return tzktData
 }
