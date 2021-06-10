8d98d9172c4646b88ed3eddbcf108e37d6276bbc
kylegrover <kyle@kylegrover.com>
2021-06-10 16:17:36 -0700

    implement react-pdf

--- src/components/media-types/pdf/index.js
+++ src/components/media-types/pdf/index.js
@@ -1,14 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './styles.module.scss'
+import { Document, Page, pdfjs } from 'react-pdf'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-export const PdfComponent = ({ src }) => (
-  <div className={styles.container}>
-    <iframe
-      title="hic et nunc PDF renderer"
-      src={`${src}#zoom=50`}
-      scrolling="no" 
-      loading="lazy"
-      sandbox="allow-scripts"
-    />
-  </div>
-)
+export const PdfComponent = ({ src, interactive }) => {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  function changePage(offset) {
+    setPageNumber(prevPageNumber => prevPageNumber + offset);
+  }
+
+  function previousPage() {
+    changePage(-1);
+  }
+
+  function nextPage() {
+    changePage(1);
+  }
+
+  let showPdfNav = interactive;
+
+  return (
+    <div className={styles.container}>
+      <Document 
+        file={src}
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} />
+        {showPdfNav &&
+          <div className={styles.pdfNav}>
+            <button
+              type="button"
+              disabled={pageNumber <= 1}
+              onClick={previousPage}
+            >
+              Previous
+            </button>
+            <p>
+              Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
+            </p>
+            <button
+              type="button"
+              disabled={pageNumber >= numPages}
+              onClick={nextPage}
+            >
+              Next
+            </button>
+          </div>
+        }
+      </Document>
+    </div>
+  )
+}
