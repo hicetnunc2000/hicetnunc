a14c0ef8ad42dfdf067a1cd0b7f42a16affcbc14
crzypatchwork <costabraba93@gmail.com>
2021-06-13 21:42:07 -0300

    revert subjkt

---
+++ src/pages/objkt-display/tabs/history.js
@@ -0,0 +1,7 @@
+import React, { useContext } from 'react'
+import { Container, Padding } from '../../../components/layout'
+import { HicetnuncContext } from '../../../context/HicetnuncContext'
+
+export const History = () => {
+    
+}
