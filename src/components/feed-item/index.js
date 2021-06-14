93e1038720c3a11e12e62001b0f80dacd9c5376b
Andre Venancio <info@andrevenancio.com>
2021-06-14 18:39:51 +0100

    allow GLB OBJKT to render on feeds.

--- src/components/feed-item/index.js
+++ src/components/feed-item/index.js
@@ -8,8 +8,16 @@ import { VisuallyHidden } from '../visually-hidden'
 import styles from './styles.module.scss'
 
 export const FeedItem = (props) => {
-  const { display_uri, id, mime, creator_id, type, artifact_uri, token_holders } = props
-/*   console.log(props)
+  const {
+    display_uri,
+    id,
+    mime,
+    creator_id,
+    type,
+    artifact_uri,
+    token_holders,
+  } = props
+  /*   console.log(props)
   console.log(type)
   console.log(mime) */
   // const url =
@@ -17,29 +25,38 @@ export const FeedItem = (props) => {
   //     ? token_info.displayUri
   //     : token_info.artifactUri
 
+  /*
+  NOTE:
+  crzy version from https://github.com/hicetnunc2000/hicetnunc/commit/6f91bde84c7a9f4adc9e4d869322b520890abd88
+  mime === 'video/mp4'
+                    ? artifact_uri.split('//')[1]
+                    : display_uri !== ''
+                    ? display_uri.split('//')[1]
+                    : artifact_uri.split('//')[1],
+   
+   */
+
   return (
     <>
       <Padding>
-
         <div>
           <Button to={`${PATH.OBJKT}/${id}`}>
             {/* <VisuallyHidden>{`Go to OBJKT: ${token_info.name}`}</VisuallyHidden> */}
             <div className={styles.container}>
               {renderMediaType({
                 mimeType: mime,
-                uri: mime == 'video/mp4' ? artifact_uri.split('//')[1] : (display_uri !== '' ? display_uri.split('//')[1] : artifact_uri.split('//')[1]),
+                uri: artifact_uri.split('//')[1],
                 metadata: props,
               })}
             </div>
           </Button>
         </div>
         <ItemInfo
-          id = {id}
-          token_holders = {token_holders}
-          creator_id = {creator_id}
+          id={id}
+          token_holders={token_holders}
+          creator_id={creator_id}
         />
       </Padding>
     </>
-
   )
 }
