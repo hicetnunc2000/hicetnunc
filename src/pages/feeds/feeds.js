c614483228306574b28af7e818da5bfffd514433
crzypatchwork <costabraba93@gmail.com>
2021-06-29 13:23:21 -0300

    latest

--- src/pages/feeds/feeds.js
+++ src/pages/feeds/feeds.js
@@ -159,7 +159,7 @@ export const Feeds = ({ type }) => {
         </Container>
       </InfiniteScroll>
       <BottomBanner>
-        The dApp has been temporarily disabled for a contract migration. Follow <a href="https://twitter.com/hicetnunc2000" target="_blank">@hicetnunc2000</a> or <a href="https://discord.gg/jKNy6PynPK" target="_blank">join the discord</a> for updates.
+      Collecting has been temporarily disabled. Follow <a href="https://twitter.com/hicetnunc2000" target="_blank">@hicetnunc2000</a> or <a href="https://discord.gg/jKNy6PynPK" target="_blank">join the discord</a> for updates.
       </BottomBanner>
     </Page>
   )
