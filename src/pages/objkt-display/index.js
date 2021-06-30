6f972f3ab3e8497352e654886e4a8d9d2ba71b48
crzypatchwork <costabraba93@gmail.com>
2021-06-30 17:14:28 -0300

    Revert "Revert "fix different video format displays, make interactive bigger""

--- src/pages/objkt-display/index.js
+++ src/pages/objkt-display/index.js
@@ -117,7 +117,16 @@ export const ObjktDisplay = () => {
             width: '100%'
           }}
           className="objkt-display">
-            <div className={nft.mime == 'video/mp4' || nft.mime == 'application/pdf' ? 'no-fullscreen' : styles.objktview}>
+            <div className={
+              nft.mime == 'application/x-directory' ? 'objktview-html objktview ' + styles.objktview:
+              [(
+                nft.mime == 'video/mp4' ||
+                nft.mime == 'video/ogv' ||
+                nft.mime == 'video/quicktime' ||
+                nft.mime == 'video/webm' ||
+                nft.mime == 'application/pdf' ? 'no-fullscreen' : 'objktview ' + styles.objktview
+              )]
+            }>
               {renderMediaType({
                 mimeType: nft.mime,
                 artifactUri: nft.artifact_uri,
