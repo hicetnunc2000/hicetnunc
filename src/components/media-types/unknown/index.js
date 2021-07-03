189642bf8e2f652bab76ec3880cdd2966b6ee9c5
crzypatchwork <costabraba93@gmail.com>
2021-07-03 14:55:18 -0300

    revert queue

--- src/components/media-types/unknown/index.js
+++ src/components/media-types/unknown/index.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 import styles from './styles.module.scss'
 const axios = require('axios')
 
-export const UnknownComponent = async ({ mimeType }) => {
-  const [queue, updateQueue] = useState()
+export const UnknownComponent = ({ mimeType }) => {
+/*   const [queue, updateQueue] = useState()
   updateQueue(await axios.post('https://api.hicdex.com/status.json').then(res => res.data))
-
+ */
   return (
     <div className={styles.container}>
-      <div className={styles.square}>{JSON.stringify(queue)}</div>
+      <div className={styles.square}>Metadata on queue</div>
     </div>
   )
 }
