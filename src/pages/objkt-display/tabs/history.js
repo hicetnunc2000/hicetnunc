a2f8c29ba3869836df0fd9201363b5f4e478d999
crzypatchwork <costabraba93@gmail.com>
2021-07-05 11:30:36 -0300

    v1 history (no css)

--- src/pages/objkt-display/tabs/history.js
+++ src/pages/objkt-display/tabs/history.js
@@ -2,6 +2,19 @@ import React, { useContext } from 'react'
 import { Container, Padding } from '../../../components/layout'
 import { HicetnuncContext } from '../../../context/HicetnuncContext'
 
-export const History = () => {
-    
+export const History = (token_info) => {
+    console.log(token_info)
+    return (
+        <div>
+{/*             {
+                token_info.trades.map(e => {
+                    return(
+                        <div>
+                            {e.timestamp} {e.amount}x {e.swap.price} {e.seller.address} {e.buyer.address} 
+                        </div>
+                    )
+                })
+            } */}
+        </div>
+    )
 }
