d6e2e0a82c1420a8f5b2174b6eded6f433526949
ztepler <ztepler@gmail.com>
2021-07-26 02:54:20 +0300

    registry call too

--- src/context/HicetnuncContext.js
+++ src/context/HicetnuncContext.js
@@ -515,7 +515,8 @@ class HicetnuncContextProviderClass extends Component {
 
       registry: async (alias, metadata) => {
         console.log(metadata)
-        return await Tezos.wallet.at(this.state.subjkt).then((c) =>
+        const subjktAddressOrProxy = this.state.proxyAddress || this.state.subjkt
+        return await Tezos.wallet.at(subjktAddressOrProxy).then((c) =>
           c.methods
             .registry(
               ('ipfs://' + metadata.path)
