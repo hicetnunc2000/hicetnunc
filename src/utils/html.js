377e502918a9c39c1fb5d844158fcfa56b5d0c76
0x00000010 <hex.ten@protonmail.com>
2021-07-29 00:34:51 +0700

    Add smartpy mainnet node in connect-src

--- src/utils/html.js
+++ src/utils/html.js
@@ -179,6 +179,7 @@ export function injectCSPMetaTagIntoHTML(html) {
       wss:
       bootstrap.libp2p.io
       preload.ipfs.io
+      https://mainnet.smartpy.io
       https://api.etherscan.io
       https://api.thegraph.com
       https://*.tzkt.io
