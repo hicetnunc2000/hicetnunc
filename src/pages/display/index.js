420883bd7fbef0fd2b26bcd8ce2b1d623b69a20a
kylegrover <kyle@kylegrover.com>
2021-09-19 19:20:49 -0700

    feathericons copy icon

--- src/pages/display/index.js
+++ src/pages/display/index.js
@@ -603,7 +603,11 @@ export default class Display extends Component {
                     data-position={'top'}
                     data-tooltip="blah">
                     <Button onClick={()=>{navigator.clipboard.writeText(this.state.wallet)}}>
-                      <Primary>🔗</Primary>
+                      <Primary>
+                        <span data-position={'top'} data-tooltip={'copy'} className={styles.top}>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-copy"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"/><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"/></svg>
+                        </span>
+                      </Primary>
                     </Button>
                   </div>
                 </div>
