46166c1da5d60cc0031f46f9f27d30d537c0a653
DESKTOP-Q8RBGJI\Violeta <violetforest5@protonmail.com>
2021-09-23 22:13:08 -0400

    clean up before PR

--- src/context/HicetnuncContext.js
+++ src/context/HicetnuncContext.js
@@ -391,10 +391,6 @@ class HicetnuncContextProviderClass extends Component {
       },
 
       collect: async (swap_id, amount) => {
-        console.log('collectv2')
-        console.log('swap_id ' + swap_id)
-        console.log('xtz ' + amount)
-        console.log('collectv2')
         return await Tezos.wallet
           .at(this.state.proxyAddress || this.state.v2)
           .then((c) =>
