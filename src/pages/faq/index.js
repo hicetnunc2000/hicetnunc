2d26192a184ad9969e6dd617a8c4ae32e7dd09b7
Pixel10312 <61020102+Pixel10312@users.noreply.github.com>
2021-09-25 15:50:09 -0400

    Initial

--- src/pages/faq/index.js
+++ src/pages/faq/index.js
@@ -22,82 +22,101 @@ export class FAQ extends Component {
       <Page title="faq" large>
         <Container>
           <Padding>
-            <div className={styles.buttons}>
-              <Button href="https://github.com/hicetnunc2000/hicetnunc/wiki/Getting-Started-with-Tezos">
-                <Primary>
-                  <strong>how do I get tezos/make a wallet?</strong>
-                </Primary>
-              </Button>
-            </div>
-            <div className={styles.buttons}>
-              <Button href="https://github.com/hicetnunc2000/hicetnunc/wiki/Introduction">
-                <Primary>
-                  <strong>how do I mint?</strong>
-                </Primary>
-              </Button>
-            </div>
-            <div className={styles.buttons}>
-              <Button href="https://github.com/hicetnunc2000/hicetnunc/wiki/Edit-your-profile">
-                <Primary>
-                  <strong>how do I edit my profile?</strong>
-                </Primary>
-              </Button>
-            </div>
-            <div className={styles.buttons}>
-              <Button href="https://github.com/hicetnunc2000/hicetnunc/wiki/How-to-swap-%F0%9F%94%83">
-                <Primary>
-                  <strong>how do I add/change the price of my OBJKT?</strong>
-                </Primary>
-              </Button>
-            </div>
-            <div className={styles.buttons}>
-              <Button href="https://github.com/hicetnunc2000/hicetnunc/wiki/How-to-burn-%F0%9F%94%A5">
-                <Primary>
-                  <strong>how do I burn my OBJKT?</strong>
-                </Primary>
-              </Button>
-            </div>
-            <div className={styles.buttons}>
-              <Button href="https://github.com/hicetnunc2000/hicetnunc/wiki/How-to-resell-%F0%9F%8F%AA">
-                <Primary>
-                  <strong>how do I resell an OBJKT?</strong>
-                </Primary>
-              </Button>
-            </div>
-            <div className={styles.buttons}>
-              <Button href="https://github.com/hicetnunc2000/hicetnunc/wiki/hDAO">
-                <Primary>
-                  <strong>what is hDAO ○?</strong>
-                </Primary>
-              </Button>
+            <div className={styles.faq__outer__container}>
+              <h1 className={styles.faq__title}>FAQ</h1>
+              <ul className={styles.faq__container}>
+                <li className={styles.buttons}>
+                  <Button href="https://github.com/hicetnunc2000/hicetnunc/wiki/Getting-Started-with-Tezos">
+                    <Primary>
+                      how do I get tezos/make a wallet?
+                    </Primary>
+                  </Button>
+                </li>
+                <li className={styles.buttons}>
+                  <Button href="https://github.com/hicetnunc2000/hicetnunc/wiki/Introduction">
+                    <Primary>
+                      how do I mint?
+                    </Primary>
+                  </Button>
+                </li>
+                <li className={styles.buttons}>
+                  <Button href="https://github.com/hicetnunc2000/hicetnunc/wiki/Edit-your-profile">
+                    <Primary>
+                      how do I edit my profile?
+                    </Primary>
+                  </Button>
+                </li>
+                <li className={styles.buttons}>
+                  <Button href="https://github.com/hicetnunc2000/hicetnunc/wiki/How-to-swap-%F0%9F%94%83">
+                    <Primary>
+                      how do I add/change the price of my OBJKT?
+                    </Primary>
+                  </Button>
+                </li>
+                <li className={styles.buttons}>
+                  <Button href="https://github.com/hicetnunc2000/hicetnunc/wiki/How-to-burn-%F0%9F%94%A5">
+                    <Primary>
+                      how do I burn my OBJKT?
+                    </Primary>
+                  </Button>
+                </li>
+                <li className={styles.buttons}>
+                  <Button href="https://github.com/hicetnunc2000/hicetnunc/wiki/How-to-resell-%F0%9F%8F%AA">
+                    <Primary>
+                      how do I resell an OBJKT?
+                    </Primary>
+                  </Button>
+                </li>
+                <li className={styles.buttons}>
+                  <Button href="https://github.com/hicetnunc2000/hicetnunc/wiki/hDAO">
+                    <Primary>
+                      what is hDAO ○?
+                    </Primary>
+                  </Button>
+                </li>
+              </ul>
             </div>
           </Padding>
         </Container>
+
         <Container>
           <Padding>
-            <div className={styles.buttons}>
-              <Button href="https://github.com/hicetnunc2000/hicetnunc/wiki/General">
-                <Primary>
-                  <strong>general faq</strong>
-                </Primary>
-              </Button>
-            </div>
-            <div className={styles.buttons}>
-              <Button href="https://github.com/hicetnunc2000/hicetnunc/wiki/Troubleshooting">
-                <Primary>
-                  <strong>troubleshooting</strong>
-                </Primary>
-              </Button>
-            </div>
-            <div className={styles.buttons}>
-              <Button href="https://github.com/hicetnunc2000/hicetnunc/wiki/Tools-made-by-the-community">
-                <Primary>
-                  <strong>useful tools</strong>
-                </Primary>
-              </Button>
+            <div className={styles.faq__outer__container}>
+              <hr className={styles.divider}/>
             </div>
           </Padding>
         </Container>
+
+        <Container>
+          <Padding>
+            <div className={styles.faq__outer__container}>
+              <ul className={styles.faq__container}>
+                <li className={styles.buttons}>
+                  <Button href="https://github.com/hicetnunc2000/hicetnunc/wiki/General">
+                    <Primary>
+                      general faq
+                    </Primary>
+                  </Button>
+                </li>
+                <li className={styles.buttons}>
+                  <Button href="https://github.com/hicetnunc2000/hicetnunc/wiki/Troubleshooting">
+                    <Primary>
+                      troubleshooting
+                    </Primary>
+                  </Button>
+                </li>
+                <li className={styles.buttons}>
+                  <Button href="https://github.com/hicetnunc2000/hicetnunc/wiki/Tools-made-by-the-community">
+                    <Primary>
+                      useful tools
+                    </Primary>
+                  </Button>
+                </li>
+              </ul>
+            </div>
+          </Padding>
+        </Container>
+
         {/*         <Container>
           <Padding>
             <div className={styles.buttons}>
