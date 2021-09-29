62cc5b3789ab45aae1a457b141cf870afb72f8ac
crzypatchwork <180111973@aluno.unb.br>
2021-09-29 02:59:03 -0300

    rpc

--- src/context/HicetnuncContext.js
+++ src/context/HicetnuncContext.js
@@ -32,8 +32,8 @@ const createProxySchema = `
 // This should be moved to a service so it is only done once on page load
 //const Tezos = new TezosToolkit('https://api.tez.ie/rpc/mainnet')
 //const Tezos = new TezosToolkit('https://mainnet-tezos.giganode.io')
-//const Tezos = new TezosToolkit('https://mainnet.smartpy.io')
-const Tezos = new TezosToolkit('https://mainnet.api.tez.ie')
+const Tezos = new TezosToolkit('https://mainnet.smartpy.io')
+//const Tezos = new TezosToolkit('https://mainnet.api.tez.ie')
 //const Tezos = new TezosToolkit('https://api.tez.ie/rpc/mainnet')
 // storage fee adjustment
 
