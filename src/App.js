14ab7f703b47456180b2229e698c59ff79d0d619
crzypatchwork <180111973@aluno.unb.br>
2022-01-25 22:14:14 -0300

    browser router test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, BrowserRouter, HashRouter } from 'react-router-dom'
 import HicetnuncContextProvider from './context/HicetnuncContext'
 import { getInitialData } from './data/api'
 import { Header } from './components/header'
 import { Loading as Preloading } from './components/loading'
 import { FeedbackComponent } from './components/feedback'
 import { routes } from './routes'
+import { Galleries } from './pages/galleries'
+import { Config } from './pages/config'
+import { Search } from './pages/search'
+import { Latest, Hdao, Random, Featured } from './pages/feeds'
+import Display from './pages/display'
+import { About } from './pages/about'
+import { FAQ } from './pages/faq'
+import { CollabDisplay, Collaborate } from './pages/collaborate'
+import { ObjktDisplay } from './pages/objkt-display'
+import { GalleryDetail } from './pages/gallery-detail'
 
 const App = () => {
   const [loading, setLoading] = useState(true)
@@ -25,11 +35,13 @@ const App = () => {
     <HicetnuncContextProvider>
       <Header />
       <FeedbackComponent />
-      <Switch>
-        {routes.map(({ exact, path, component: Comp }) => (
-          <Route path={path} exact={exact} key={path} component={Comp} />
-        ))}
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          {routes.map(({ exact, path, component: Comp }) => (
+            <Route path={path} exact={exact} key={path} component={Comp} />
+          ))}
+        </Switch>
+      </BrowserRouter>
     </HicetnuncContextProvider>
   )
 }
